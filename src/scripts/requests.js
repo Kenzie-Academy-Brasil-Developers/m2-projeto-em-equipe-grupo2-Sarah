c7cbc6260b81ab6c5b795077a3826fb16b48f4c3
Julia Otomo Duarte <julia_otomoduarte@outlook.com>
2023-01-16 19:52:51 -0300

    first commit

---
+++ src/scripts/requests.js
@@ -0,0 +1,4 @@
+const requestsHeader = {
+    'X-RapidAPI-Key': 'dfbf9a3fdfmsh0e58794cb87a218p16186bjsn15d4aea76006',
+    'X-RapidAPI-Host': 'vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com'
+}
