fbff154bf3a1606954c3232d0e0af14a473842f2
igor <igorbragafarais@gmail.com>
2023-01-19 21:03:13 -0300

    versao att

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -794,7 +794,9 @@ function startTimer(duration, display) {
     seconds;
   setInterval(function () {
       minutes = parseInt(timer / 60, 10);
+      console.log(minutes)
       seconds = parseInt(timer % 60, 10);
+      console.log(seconds)
 
     minutes = minutes < 10 ? "0" + minutes : minutes;
     seconds = seconds < 10 ? "0" + seconds : seconds;
